3d695bb4afa30122c69fb43ac6d04806ad4ae0e4
Daniel Andre Eikeland <zegenie@gmail.com>
2020-10-14 09:45:46 +0200

    wip user popup improvements

--- core/modules/main/components/usercard.php
+++ core/modules/main/components/usercard.php
@@ -3,13 +3,12 @@
     /** @var \pachno\core\entities\User $user */
 
 ?>
-<div class="backdrop_box x-large usercard" id="user_details_popup">
+<div class="backdrop_box medium usercard" id="user_details_popup">
     <div class="backdrop_detail_header">
         <div class="avatar-container">
             <?php echo image_tag($user->getAvatarURL(false), array('alt' => ' '), true); ?>
         </div>
-        <span><?php echo (!$user->isScopeConfirmed()) ? $user->getUsername() : $user->getRealname() . '(' . $user->getUsername() . ')'; ?></span>
-        <a href="javascript:void(0);" class="closer" onclick="Pachno.UI.Backdrop.reset();"><?= fa_image_tag('times'); ?></a>
+        <span><?php echo (!$user->isScopeConfirmed()) ? $user->getUsername() : $user->getRealname(); ?></span>
     </div>
     <div id="backdrop_detail_content" class="backdrop_detail_content">
         <?php if (!$user->isScopeConfirmed()): ?>
@@ -20,16 +19,24 @@
                 </div>
             </div>
         <?php else: ?>
-            <div class="user_profile">
-                <div class="user_realname">
-                    <?php echo $user->getRealname(); ?> <span class="user_username">(<?php echo $user->getUsername(); ?>)</span>
-                    <div class="user_status"><?php echo pachno_get_userstate_image($user) . __($user->getState()->getName()); ?></div>
-                    <?php if ($user->isEmailPublic() || $pachno_user->canAccessConfigurationPage(\pachno\core\framework\Settings::CONFIGURATION_SECTION_USERS)): ?>
-                        <div class="user_email"><?php echo link_tag('mailto:'.$user->getEmail(), $user->getEmail()); ?></div>
-                    <?php endif; ?>
-                    <?php if (\pachno\core\entities\User::isThisGuest() == false): ?>
-                        <div id="friends_message_<?php echo $user->getUsername() . '_' . $rnd_no; ?>" style="padding: 10px 0 0 0; font-size: 0.75em;"></div>
-                        <?php if ($user->getID() != \pachno\core\framework\Context::getUser()->getID() && !(\pachno\core\framework\Context::getUser()->isFriend($user)) && !$user->isGuest()): ?>
+            <div class="list-mode">
+                <div class="list-item">
+                    <?= fa_image_tag('at', ['class' => 'icon']); ?>
+                    <span class="name"><?php echo $user->getUsername(); ?></span>
+                </div>
+                <div class="list-item">
+                    <span class="icon"><?php echo pachno_get_userstate_image($user); ?></span>
+                    <span class="name"><?= __($user->getState()->getName()); ?></span>
+                </div>
+                <?php if ($user->isEmailPublic() || $pachno_user->canAccessConfigurationPage(\pachno\core\framework\Settings::CONFIGURATION_SECTION_USERS)): ?>
+                    <div class="list-item">
+                        <?= fa_image_tag('envelope', ['class' => 'icon']); ?>
+                        <span class="name"><?php echo link_tag('mailto:'.$user->getEmail(), $user->getEmail()); ?></span>
+                    </div>
+                <?php endif; ?>
+                <?php if (\pachno\core\entities\User::isThisGuest() == false): ?>
+                    <div id="friends_message_<?php echo $user->getUsername() . '_' . $rnd_no; ?>" style="padding: 10px 0 0 0; font-size: 0.75em;"></div>
+                    <?php if ($user->getID() != \pachno\core\framework\Context::getUser()->getID() && !(\pachno\core\framework\Context::getUser()->isFriend($user)) && !$user->isGuest()): ?>
                             <div id="friends_link_<?php echo $user->getUsername() . '_' . $rnd_no; ?>" class="friends_link">
                         <span style="padding: 2px; <?php if (\pachno\core\framework\Context::getUser()->isFriend($user)): ?> display: none;<?php endif; ?>" id="add_friend_<?php echo $user->getID() . '_' . $rnd_no; ?>">
                             <?php echo javascript_link_tag(__('Become friends'), array('onclick' => "Pachno.Main.Profile.addFriend('".make_url('toggle_friend', array('mode' => 'add', 'user_id' => $user->getID()))."', {$user->getID()}, {$rnd_no});")); ?>
@@ -39,70 +46,76 @@
                             <?php echo javascript_link_tag(__('Remove this friend'), array('onclick' => "Pachno.Main.Profile.removeFriend('".make_url('toggle_friend', array('mode' => 'remove', 'user_id' => $user->getID()))."', {$user->getID()}, {$rnd_no});")); ?>
                         </span>
                             </div>
-                        <?php endif; ?>
                     <?php endif; ?>
-                    <?php if ($pachno_user->canAccessConfigurationPage(\pachno\core\framework\Settings::CONFIGURATION_SECTION_USERS)): ?>
-                        <div class="edit_user">
-                            <form action="<?php echo make_url('configure_users'); ?>">
-                                <input type="hidden" name="finduser" value="<?php echo $user->getUsername(); ?>">
-                                <a href="javascript:void(0);" onclick="$(this).up('form').submit();"><?php echo __('Edit this user'); ?></a>
-                            </form>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <div class="user_details">
+                <?php endif; ?>
+                <?php if ($pachno_user->canAccessConfigurationPage(\pachno\core\framework\Settings::CONFIGURATION_SECTION_USERS)): ?>
+                    <div class="list-item">
+                        <form action="<?php echo make_url('configure_users'); ?>">
+                            <input type="hidden" name="finduser" value="<?php echo $user->getUsername(); ?>">
+                            <a href="javascript:void(0);" onclick="$(this).up('form').submit();"><?php echo __('Edit this user'); ?></a>
+                        </form>
+                    </div>
+                <?php endif; ?>
                 <?php if (!$user->getJoinedDate()): ?>
-                    <i><?php echo __('This user has been a member for a while'); ?></i>
+                    <div class="list-item disabled">
+                        <span class="name"><?php echo __('This user has been a member for a while'); ?></span>
+                    </div>
                 <?php else: ?>
-                    <?php echo '<b>' . __('This user has been a member since %date', array('%date' => '</b>' . \pachno\core\framework\Context::getI18n()->formatTime($user->getJoinedDate(), 11))); ?>
+                    <div class="list-item">
+                        <span class="name"><?php echo __('This user has been a member since %date', ['%date' => \pachno\core\framework\Context::getI18n()->formatTime($user->getJoinedDate(), 11)]); ?></span>
+                    </div>
                 <?php endif; ?>
-                <br>
                 <?php if (!$user->getLastSeen()): ?>
-                    <i><?php echo __('This user has not logged in yet'); ?></i>
+                    <div class="list-item disabled">
+                        <span class="name"><?php echo __('This user has not logged in yet'); ?></span>
+                    </div>
                 <?php else: ?>
-                    <?php echo '<b>' . __('This user was last seen online at %time', array('%time' => '</b>' . \pachno\core\framework\Context::getI18n()->formatTime($user->getLastSeen(), 11))); ?>
+                    <div class="list-item">
+                        <span class="name"><?php echo __('This user was last seen online at %time', ['%time' => \pachno\core\framework\Context::getI18n()->formatTime($user->getLastSeen(), 11)]); ?></span>
+                    </div>
                 <?php endif; ?>
-                <br>
                 <?php if (!$user->getLatestActions(1)): ?>
-                    <i><?php echo __('There is no recent activity available for this user'); ?></i>
+                    <div class="list-item disabled">
+                        <span class="name"><?php echo __('There is no recent activity available for this user'); ?></span>
+                    </div>
                 <?php else: ?>
                     <?php foreach ($user->getLatestActions(1) as $action): ?>
-                        <?php echo '<b>' . __('Last user activity was at %time', array('%time' => '</b>' . \pachno\core\framework\Context::getI18n()->formatTime($action->getTime(), 11))); ?>
+                        <div class="list-item">
+                            <span class="name"><?php echo __('Last user activity was at %time', ['%time' => \pachno\core\framework\Context::getI18n()->formatTime($action->getTime(), 11)]); ?></span>
+                        </div>
                     <?php endforeach; ?>
                 <?php endif; ?>
-                <br>
+                <div class="header">
+                    <h3><?= __('Recently reported issues'); ?></h3>
+                </div>
                 <?php if (count($issues)): ?>
                     <?php echo __('This user has reported %issues issue(s)', array('%issues' => '<b>'.count($issues).'</b>')); ?>
                     <?php echo link_tag(make_url('search', array('search' => true, 'fs[posted_by]' => array('o' => '=', 'v' => $user->getID()))), __('Show issues'), array('class' => 'button', 'title' => __('Show issues reported by this user'))); ?>
                     <?php $seen = 0; ?>
-                    <h4><?php echo __('Last reported issues:') . ' '; ?></h4>
-                        <ul class="simple-list user_issues_list">
-                        <?php foreach ($issues as $issue): ?>
-                            <?php if ($issue->hasAccess()): ?>
-                                <li>
-                                    <span class="faded_out smaller"><?php echo link_tag(make_url('project_dashboard', array('project_key' => $issue->getProject()->getKey())), image_tag($issue->getProject()->getIconName(), array('class' => 'issuelog-project-logo'), true)); ?></span>
-                                    <?php echo link_tag(make_url('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo())), pachno_truncateText($issue->getFormattedTitle(true), 100)); ?>
-                                </li>
-                                <?php if (++$seen == 7) break; ?>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                        </ul>
+                    <?php foreach ($issues as $issue): ?>
+                        <?php if ($issue->hasAccess()): ?>
+                            <a class="list-item" href="<?= make_url('project_dashboard', array('project_key' => $issue->getProject()->getKey())); ?>">
+                                <?= image_tag($issue->getProject()->getIconName(), ['class' => 'icon issuelog-project-logo'], true); ?>
+                                <span class="name"><?php echo pachno_truncateText($issue->getFormattedTitle(true), 100); ?></span>
+                            </a>
+                            <?php if (++$seen == 7) break; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
                 <?php else: ?>
-                    <i><?php echo __('This user has not reported any issues yet'); ?></i>
+                    <div class="list-item disabled"><span class="name"><?php echo __('This user has not reported any issues yet'); ?></span></div>
                 <?php endif; ?>
-                <br>
                 <?php if (count($user->getTeams())): ?>
-                    <b><?php echo __('Member of the following teams:</b> %list_of_teams', array('%list_of_teams' => ''), true); ?></b><br>
-                    <ul class="teamlist">
-                        <?php foreach ($user->getTeams() as $team): ?>
-                            <li><?php include_component('main/teamdropdown', array('team' => $team)); ?></li>
-                        <?php endforeach; ?>
-                    </ul>
+                    <div class="list-item header">
+                        <h3><?= __('Team memberships'); ?></h3>
+                    </div>
+                    <?php foreach ($user->getTeams() as $team): ?>
+                        <div class="list-item"><?php include_component('main/teamdropdown', array('team' => $team)); ?></div>
+                    <?php endforeach; ?>
                 <?php endif; ?>
             </div>
             <?php \pachno\core\framework\Event::createNew('core', 'usercardactions_top', $user)->trigger(); ?>
             <?php \pachno\core\framework\Event::createNew('core', 'usercardactions_bottom', $user)->trigger(); ?>
         <?php endif; ?>
+        <button class="button secondary closer"><?= __('Close popup'); ?></button>
     </div>
 </div>
