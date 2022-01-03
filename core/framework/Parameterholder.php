8f9908c3fde0dafe7521f6bf52224bfc3a26b410
Daniel André Eikeland <zegenie@zegeniestudios.net>
2022-01-03 12:34:13 +0100

    change parameterholder declaration to remove compatibility issue

--- core/framework/Parameterholder.php
+++ core/framework/Parameterholder.php
@@ -1,9 +1,9 @@
 <?php
-
+    
     namespace pachno\core\framework;
-
+    
     use ArrayAccess;
-
+    
     /**
      * Parameter holder class used in the MVC part of the framework for \pachno\core\entities\Action and \pachno\core\entities\ActionComponent
      *
@@ -12,54 +12,54 @@
      */
     class Parameterholder implements ArrayAccess
     {
-
+        
         protected $_property_list = [];
-
+        
         public function getParameterHolder()
         {
             return $this->_property_list;
         }
-
-        public function offsetUnset(mixed $offset): void
+        
+        public function offsetUnset($offset)
         {
             if (array_key_exists($offset, $this->_property_list)) {
                 unset($this->_property_list[$offset]);
             }
         }
-
-        public function offsetSet(mixed $offset, mixed $value): void
+        
+        public function offsetSet($offset, $value)
         {
             $this->__set($offset, $value);
         }
-
-        public function offsetGet(mixed $offset): mixed
+        
+        public function offsetGet($offset)
         {
             return $this->__get($offset);
         }
-
+        
         public function __get($property)
         {
             return ($this->hasParameter($property)) ? $this->_property_list[$property] : null;
         }
-
+        
         public function __set($key, $value)
         {
             $this->_property_list[$key] = $value;
         }
-
+        
         public function hasParameter($key)
         {
             return $this->__isset($key);
         }
-
+        
         public function __isset($key)
         {
             return array_key_exists($key, $this->_property_list);
         }
-
-        public function offsetExists(mixed $offset): bool
+        
+        public function offsetExists($offset)
         {
             return $this->__isset($offset);
         }
-
+        
     }
